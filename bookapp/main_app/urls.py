c596bc0946a956e982c70a1c62394ee62bb3796d
cameron <aliyevcameron@gmail.com>
2024-08-26 13:33:35 -0400

    done

--- bookapp/main_app/urls.py
+++ bookapp/main_app/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('cart/', views.view_cart, name='view_cart'),
     path('add/<int:book_id>/', views.add_to_cart, name='add_to_cart'),
+    path('checkout/', views.view_cart, name='checkout'),
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
--- bookapp/main_app/views.py
+++ bookapp/main_app/views.py
@@ -32,8 +32,7 @@ def view_cart(request):
  
 def add_to_cart(request, book_id):
     book = Book.objects.get(id=book_id)
-    cart_item, created = CartItem.objects.get_or_create(book=book, 
-                                                       user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(book=book, user=request.user)
     cart_item.quantity += 1
     cart_item.save()
     return redirect('view_cart')
